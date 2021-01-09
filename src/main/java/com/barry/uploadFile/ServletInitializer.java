7c4da5f94984d672f3201ec8b33173128be084f8
algassby <algassby@hotmail.com>
2021-01-09 20:59:48 +0100

    project config

---
+++ src/main/java/com/barry/uploadFile/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.barry.uploadFile;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(UploadFileSpringBootJpaApplication.class);
+	}
+
+}
